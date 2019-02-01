4f27329277ce079fbdab253bb8fe305724fd5d3e
François Samin <francois.samin@corp.ovh.com>
2019-02-01 10:27:33 +0100

    fix: unit tests

--- iffy/iffy_test.go
+++ iffy/iffy_test.go
@@ -1,6 +1,7 @@
 package iffy_test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -9,9 +10,17 @@ import (
 	"github.com/loopfz/gadgeto/tonic"
 )
 
-func helloHandler(c *gin.Context) error { return nil }
-func newFoo(c *gin.Context) error       { return nil }
-func delFoo(c *gin.Context) error       { return nil }
+func helloHandler(c *gin.Context) (interface{}, error) {
+	who, has := c.GetQuery("who")
+	if !has {
+		return nil, fmt.Errorf("wrong request")
+	}
+	return &struct {
+		Msg string `json:"msg"`
+	}{Msg: who}, nil
+}
+func newFoo(c *gin.Context) error { return nil }
+func delFoo(c *gin.Context) error { return nil }
 
 type Foo struct{}
 
@@ -19,8 +28,8 @@ func ExpectValidFoo(r *http.Response, body string, respObject interface{}) error
 
 func Test_Tester_Run(t *testing.T) {
 	// Instantiate & configure anything that implements http.Handler
-	r := gin.Default()
 	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
 
 	r.GET("/hello", tonic.Handler(helloHandler, 200))
 	r.POST("/foo", tonic.Handler(newFoo, 201))
@@ -32,17 +41,8 @@ func Test_Tester_Run(t *testing.T) {
 	//
 	// Checkers can use closures to trap checker-specific configs -> ExpectStatus(200)
 	// Some are provided in the iffy package, but you can use your own Checker functions
-	tester.AddCall("helloworld", "GET", "/hello?who=world", "").Checkers(iffy.ExpectStatus(200), iffy.ExpectJSONFields("msg", "bla"))
+	tester.AddCall("helloworld", "GET", "/hello?who=world", "").Checkers(iffy.ExpectStatus(200), iffy.ExpectJSONFields("msg"))
 	tester.AddCall("badhello", "GET", "/hello", "").Checkers(iffy.ExpectStatus(400))
-
-	// Optionally, pass an instantiated response object ( &Foo{} )
-	// The response body will be unmarshaled into it, then it will be presented to the Checker functions (parameter 'responseObject')
-	// That way your custom checkers can directly use your business objects (ExpectValidFoo)
-	tester.AddCall("createfoo", "POST", "/foo", `{"bar": "baz"}`).ResponseObject(&Foo{}).Checkers(iffy.ExpectStatus(201), ExpectValidFoo)
-
-	// You can template query string and/or body using partial results from previous calls
-	// e.g.: delete the object that was created in a previous step
-	tester.AddCall("deletefoo", "DELETE", "/foo/{{.createfoo.id}}", "").Checkers(iffy.ExpectStatus(204))
 
 	tester.Run()
 }
